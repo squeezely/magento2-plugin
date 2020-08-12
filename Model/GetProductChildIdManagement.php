44149cad7ff2e2a25677ce6a6b6211b54f01543b
Jonathan de Zoete <jonathan@squeezely.tech>
2020-08-12 14:40:54 +0200

    fix prices for different product types

--- Model/GetProductChildIdManagement.php
+++ Model/GetProductChildIdManagement.php
@@ -59,11 +59,35 @@ class GetProductChildIdManagement
                 $stockItem = $product->getExtensionAttributes()->getStockItem();
                 $productChildId =  $this->_catalogProductTypeConfigurable->getParentIdsByChild($id);
 
+                //get price
+                $regularPrice = $product->getPriceInfo()->getPrice('regular_price')->getValue();
+                $specialPrice = $product->getPriceInfo()->getPrice('special_price')->getValue();
+                if ($product->getTypeId() == 'configurable') {
+                    $basePrice = $product->getPriceInfo()->getPrice('regular_price');
+                    $regularPrice = $basePrice->getMinRegularAmount()->getValue();
+                    $specialPrice = $product->getFinalPrice();
+                }
+                else if ($product->getTypeId() == 'bundle') {
+                    $regularPrice = $product->getPriceInfo()->getPrice('regular_price')->getMinimalPrice()->getValue();
+                    $specialPrice = $product->getPriceInfo()->getPrice('final_price')->getMinimalPrice()->getValue();
+                }
+                elseif ($product->getTypeId() == 'grouped') {
+                    $usedProds = $product->getTypeInstance(true)->getAssociatedProducts($product);
+                    foreach ($usedProds as $child) {
+                        if ($child->getId() != $product->getId()) {
+                            $regularPrice += $child->getPrice();
+                            $specialPrice += $child->getFinalPrice();
+                        }
+                    }
+                }
+
+
                 $productData = [];
                 $productData['parent_id'] = $productChildId;
                 $productData['images'] = $productImageUrls;
                 $productData['image'] = $productImage;
-                $productData['price'] = $product->getPrice();
+                $productData['price'] = $regularPrice ?: $product->getPrice();
+                $productData['sale_price'] = $specialPrice;
                 $productData['availability'] = ($product->isAvailable() ? 'in stock' : 'out of stock');
                 $productData['inventory'] = $stockItem ? $stockItem->getQty() : 1;
                 $productData['sku'] = $id;
