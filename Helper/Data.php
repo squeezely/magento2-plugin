bfe2fffde975866c7ca052336ce0be6a3c85f264
Ali Alwash <alitrix@gmail.com>
2020-10-15 10:37:57 +0200

    SQZLY-5762 Don't show pixel is plugin is disabled in the admin panel

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,30 +1,23 @@
 <?php
 namespace Squeezely\Plugin\Helper;
 
-class Data extends \Magento\Framework\App\Helper\AbstractHelper
-{
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected $scopeConfig;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper {
 
     const XML_PATH_SQUEEZELY_ENABLED = 'squeezely_plugin/general/enabled';
     const XML_PATH_SQUEEZELY_ID = 'squeezely_plugin/general/SQZLY_id';
     const XML_PATH_SQUEEZELY_API_KEY = 'squeezely_plugin/general/squeezely_api_key';
     const XML_PATH_SQUEEZELY_WEBHOOK_KEY = 'squeezely_plugin/general/squeezely_webhook_key';
 
-    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig) {
-        $this->scopeConfig = $scopeConfig;
-    }
-
     /**
      * Check if the module is enabled
      *
      * @return boolean 0 or 1
      */
-    public function getIsEnable() {
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        return $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_ENABLED, $storeScope);
+    public function getIsEnabled() {
+        return $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_ENABLED, ScopeInterface::SCOPE_STORE);
     }
 
     /**
@@ -33,26 +26,23 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
      * @return string
      */
     public function getSQZLYId() {
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        $SQZLYId = $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_ID, $storeScope);
-        return trim($SQZLYId);
+        $squeezelyId = $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_ID, ScopeInterface::SCOPE_STORE);
+        return trim($squeezelyId);
     }
 
     /**
      * @return string
      */
     public function getSqueezelyApiKey() {
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        $SQZLYApiKey = $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_API_KEY, $storeScope);
-        return trim($SQZLYApiKey);
+        $apiKey = $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_API_KEY, ScopeInterface::SCOPE_STORE);
+        return trim($apiKey);
     }
 
     /**
      * @return string
      */
     public function getSqueezelyWebhookKey() {
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        $SQZLYWebHookKey = $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_WEBHOOK_KEY, $storeScope);
-        return trim($SQZLYWebHookKey);
+        $webhookKey = $this->scopeConfig->getValue(self::XML_PATH_SQUEEZELY_WEBHOOK_KEY, ScopeInterface::SCOPE_STORE);
+        return trim($webhookKey);
     }
 }
