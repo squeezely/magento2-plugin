35f62d30e886355fcc0eca69247fb6a5d34d6ce4
Marvin Besselsen <marvin@magmodules.eu>
2023-04-05 16:32:26 +0200

    Optimize queued events

--- view/frontend/web/js/queuedevents.js
+++ view/frontend/web/js/queuedevents.js
@@ -1,24 +1,28 @@
 define([
         'jquery',
         'uiComponent',
+        'domReady!',
     ], function ($, Component) {
         'use strict';
         return Component.extend({
             initialize: function () {
                 this._super();
-                window._sqzl = _sqzl || [];
-
-                var events = [];
-                $.ajax({
-                    url: this.queuedevents.url,
-                    type: 'post',
-                    success: function (data) {
-                        events = $.parseJSON(data);
-                        $.each(events, function(i, event) {
-                            _sqzl.push(event);
-                        });
-                    }
+                $(document).ajaxStop(() => {
+                    window._sqzl = _sqzl || [];
+                    var events = [];
+                    $.ajax({
+                        url: this.queuedevents.url,
+                        type: 'get',
+                        global: false,
+                        success: function (data) {
+                            events = $.parseJSON(data);
+                            $.each(events, function(i, event) {
+                                _sqzl.push(event);
+                            });
+                        }
+                    });
                 });
+
             },
         });
     }
